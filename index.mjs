2e086c5965657dde4c6ee238f8db54d521119e19
Fernando Reis Guimarães <fernandobhz@gmail.com>
2021-09-17 17:52:08 -0300

    working

---
+++ index.mjs
@@ -0,0 +1,34 @@
+import dedent from "dedent";
+import { join, dirname } from "path";
+import { writeFileSync } from "fs";
+import * as config from "config-get-fresh-mjs";
+
+const { log, error: consoleError } = console;
+
+const die = (...args) => {
+  consoleError(...args);
+  process.exit(1);
+};
+
+setInterval(async () => {
+  const timeout = config.get(`timeout`);
+  console.log({ timeout });
+}, 1_000);
+
+setInterval(() => {
+  const newTimeout = Math.floor(Math.random() * 100);
+  const localYamlPath = join(`config`, `local.yaml`);
+  const localYamlContent = dedent`
+    timeout: ${newTimeout}
+  `;
+  writeFileSync(localYamlPath, localYamlContent);
+  console.log({ newTimeout });
+}, 3_000);
+
+const localYamlPath = join(`config`, `local.yaml`);
+const localYamlContent = dedent`
+  timeout: 1
+`;
+writeFileSync(localYamlPath, localYamlContent);
+
+setTimeout(process.exit, 50_000, 0);
